6303e4bc471030ed0b6be3fa55c006d098e491ae
jeonghun.kim <jeonghun.kim@monolith.co.kr>
2024-06-13 23:17:18 +0900

    테스트

--- josephus/src/App.java
+++ josephus/src/App.java
@@ -5,7 +5,6 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src/input.txt");
