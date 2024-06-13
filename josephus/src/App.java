296db4760d5a9b0b824b965f31010bd6a1f3bec1
heavyuser-2023 <heavysuer73@gmail.com>
2024-06-13 23:04:44 +0900

    test

--- josephus/src/App.java
+++ josephus/src/App.java
@@ -5,6 +5,8 @@ import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.List;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src/input.txt");
