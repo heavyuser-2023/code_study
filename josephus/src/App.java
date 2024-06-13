ca421a264df46f4e5ac5537cb9d24364b4cc0939
heavyuser-2023 <heavyuser@heavyuserui-MacBookPro.local>
2024-06-13 22:55:05 +0900

    test

--- josephus/src/App.java
+++ josephus/src/App.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 
+
 public class App {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src/input.txt");
