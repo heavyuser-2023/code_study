bf2640240ef140fbc34484cbe132dd5ff3f9209f
heavyuser-2023 <jeonghun.kim@monolith.co.kr>
2024-06-13 23:07:57 +0900

    test

--- josephus/src/App.java
+++ josephus/src/App.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-
 public class App {
     public static void main(String[] args) throws Exception {
         FileReader fr = new FileReader("src/input.txt");
