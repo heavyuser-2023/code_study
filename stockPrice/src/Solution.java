e738f2bb35eee5e97931cb35521c1dd4dffe51aa
jeonghun.kim <jeonghun.kim@monolith.co.kr>
2024-07-06 16:52:50 +0900

    완료

--- stockPrice/src/Solution.java
+++ stockPrice/src/Solution.java
@@ -7,11 +7,18 @@ class Solution {
 
     public static void main(String args[]) {
 
+        // 테스트 코드 
         int [] prices = {1, 2, 3, 2, 3};
+        int [] expectedResult = {4, 3, 1, 1, 0};
 
         Solution solution = new Solution();
         
-        System.out.println(Arrays.toString(solution.solution(prices)));
+        int [] result = solution.solution(prices);
+
+        System.out.println(Arrays.toString(result));
+
+        String b = Arrays.equals(result, expectedResult)?"true":"false";
+        System.out.println("result : " + b);
 
 
     }
@@ -24,31 +31,18 @@ class Solution {
     }
     
     int [] calcRemainPositive( int [] prices) {
-        List<Stack<Integer>>  stackArray = new ArrayList<>();
-        for(int i=0;i<prices.length;i++) {
-            stackArray.add(new Stack<Integer>());
-        }
         int [] result = new int [prices.length];
-        for(int price : prices) {
-            for(Stack<Integer> stack : stackArray) {
-                if(stack.isEmpty()) {
-                    stack.push(price);
-                }
-                else if(stack.peek() != -1 && stack.peek() >= price) {
-                    stack.push(price);
-                }
-                else {
-                    stack.push(-1);
+
+        for(int i=0;i<prices.length;i++) {
+            for(int j=i+1;j<prices.length;j++) {
+
+                result[i]++;
+                if(prices[i] > prices[j]) {
+                    break;
                 }
             }
         }
-        int index=0;
-        for(Stack<Integer> stack : stackArray) {
-            if(stack.peek() == -1) {
-                stack.pop();
-            }
-            result[index++] = stack.size();
-        }
+      
         return result;
     }
 }
